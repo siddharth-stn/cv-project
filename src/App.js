4128b894a134e45bca0c41de8c7a3de20dcadacb
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-04-03 15:05:31 +0530

    Code for App.js refactored with using hooks and functional component

--- src/App.js
+++ src/App.js
@@ -1,199 +1,157 @@
 import GenInfo from "./components/GenInfo";
 import EduExp from "./components/EduExp";
 import PracExp from "./components/PracExp";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
 
-    this.state = {
-      name: "",
-      email: "",
-      phone: "",
-      school: "",
-      study: "",
-      dateOfStudy: "",
-      company: "",
-      position: "",
-      task: "",
-      dateFrom: "",
-      dateTo: "",
-      buttonClicked: false,
-      edit: false,
-      makeDisable: false,
-    };
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [school, setSchool] = useState("")
+  const [study, setStudy] = useState("")
+  const [dateOfStudy, setDateOfStudy] = useState("")
+  const [company, setCompany] = useState("")
+  const [position, setPosition] = useState("")
+  const [task, setTask] = useState("")
+  const [dateFrom, setDateFrom] = useState("")
+  const [dateTo, setDateTo] = useState("")
+  const [buttonClicked, setButtonClicked] = useState(false)
+  const [edit, setEdit] = useState(false)
+  const [makeDisable, setMakeDisable] = useState(false)
 
-    this.bringName = this.bringName.bind(this);
-    this.bringEmail = this.bringEmail.bind(this);
-    this.bringPhone = this.bringPhone.bind(this);
-    this.bringSchool = this.bringSchool.bind(this);
-    this.bringStudy = this.bringStudy.bind(this);
-    this.bringDateOfStudy = this.bringDateOfStudy.bind(this);
-    this.bringCompany = this.bringCompany.bind(this);
-    this.bringPosition = this.bringPosition.bind(this);
-    this.bringTasks = this.bringTasks.bind(this);
-    this.bringDateFrom = this.bringDateFrom.bind(this);
-    this.bringDateTo = this.bringDateTo.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
+  const state = {
+    name: name,
+    email: email,
+    phone: phone,
+    school: school,
+    study: study,
+    dateOfStudy: dateOfStudy,
+    company: company,
+    position: position,
+    task: task,
+    dateFrom: dateFrom,
+    dateTo: dateTo,
+    buttonClicked: buttonClicked,
+    edit: edit,
+    makeDisable: makeDisable,
+  };
+
+  const bringName = (e) => {
+    setName(e.target.value);
+    setButtonClicked(false);
+  };
+
+  const bringEmail = (e) => {
+    setEmail(e.target.value)
+    setButtonClicked(false)
+  };
+
+  const bringPhone = (e) => {
+    setPhone(e.target.value);
+    setButtonClicked(false);
   }
 
-  bringName(e) {
-    this.setState({
-      name: e.target.value,
-      buttonClicked: false,
-    });
+  const bringSchool = (e) => {
+    setSchool(e.target.value);
+    setButtonClicked(false);
   }
 
-  bringEmail(e) {
-    this.setState({
-      email: e.target.value,
-      buttonClicked: false,
-    });
+  const bringStudy = (e) => {
+    setStudy(e.target.value)
+    setButtonClicked(false);
   }
 
-  bringPhone(e) {
-    this.setState({
-      phone: e.target.value,
-      buttonClicked: false,
-    });
+  const bringDateOfStudy = (e) => {
+    setDateOfStudy(e.target.value)
+    setButtonClicked(false);
   }
 
-  bringSchool(e) {
-    this.setState({
-      school: e.target.value,
-      buttonClicked: false,
-    });
+  const bringCompany = (e) => {
+    setCompany(e.target.value);
+    setButtonClicked(false);
   }
 
-  bringStudy(e) {
-    this.setState({
-      study: e.target.value,
-      buttonClicked: false,
-    });
+  const bringPosition = (e) => {
+    setPosition(e.target.value);
+    setButtonClicked(false);
   }
 
-  bringDateOfStudy(e) {
-    this.setState({
-      dateOfStudy: e.target.value,
-      buttonClicked: false,
-    });
+  const bringTasks = (e) => {
+    setTask(e.target.value);
+    setButtonClicked(false);
   }
 
-  bringCompany(e) {
-    this.setState({
-      company: e.target.value,
-      buttonClicked: false,
-    });
+  const bringDateFrom = (e) => {
+    setDateFrom(e.target.value);
+      setButtonClicked(false);
   }
 
-  bringPosition(e) {
-    this.setState({
-      position: e.target.value,
-      buttonClicked: false,
-    });
+  const bringDateTo = (e) => {
+    setDateTo (e.target.value);
+      setButtonClicked(false);
   }
 
-  bringTasks(e) {
-    this.setState({
-      task: e.target.value,
-      buttonClicked: false,
-    });
+  const handleEdit = () => {
+    setButtonClicked(false);
+    setEdit(true);
+    setMakeDisable(false);
   }
 
-  bringDateFrom(e) {
-    this.setState({
-      dateFrom: e.target.value,
-      buttonClicked: false,
-    });
+  const handleSubmit = () => {
+    setButtonClicked(true);
+    setMakeDisable(true);
   }
 
-  bringDateTo(e) {
-    this.setState({
-      dateTo: e.target.value,
-      buttonClicked: false,
-    });
-  }
-
-  handleEdit() {
-    this.setState({
-      buttonClicked: false,
-      edit: true,
-      makeDisable: false,
-    });
-  }
-
-  handleSubmit() {
-    this.setState({
-      buttonClicked: true,
-      makeDisable: true,
-    });
-  }
-
-  render() {
-    const {
-      name,
-      email,
-      phone,
-      school,
-      study,
-      dateOfStudy,
-      company,
-      position,
-      task,
-      dateFrom,
-      dateTo,
-    } = this.state;
-
-    if (this.state.buttonClicked == true) {
-      var element = (
-        <div>
-          <h1>My Name is: {name}</h1>
-          <h1>My Email is: {email}</h1>
-          <h1>My Phone Number is: {phone}</h1>
-          <h1>My School Name is: {school}</h1>
-          <h1>My Study is: {study}</h1>
-          <h1>My date of Study is: {dateOfStudy}</h1>
-          <h1>My Company Name is: {company}</h1>
-          <h1>My Position Title is: {position}</h1>
-          <h1>The task completed is: {task}</h1>
-          <h1>Date from: {dateFrom}</h1>
-          <h1>Date To: {dateTo}</h1>
-        </div>
-      );
-    }
-
-    return (
+  if (buttonClicked == true) {
+    var element = (
       <div>
-        <div>
-          <GenInfo
-            dataName={this.bringName}
-            dataEmail={this.bringEmail}
-            dataPhone={this.bringPhone}
-            state={this.state}
-          />
-          <EduExp
-            dataSchool={this.bringSchool}
-            dataStudy={this.bringStudy}
-            dataDateOfStudy={this.bringDateOfStudy}
-            state={this.state}
-          />
-          <PracExp
-            dataCompany={this.bringCompany}
-            dataPosition={this.bringPosition}
-            dataTasks={this.bringTasks}
-            dataDateFrom={this.bringDateFrom}
-            dataDateTo={this.bringDateTo}
-            state={this.state}
-          />
-        </div>
-        <button onClick={this.handleEdit}>Edit</button>
-        <button onClick={this.handleSubmit}>Submit</button>
-        {element}
+        <h1>My Name is: {name}</h1>
+        <h1>My Email is: {email}</h1>
+        <h1>My Phone Number is: {phone}</h1>
+        <h1>My School Name is: {school}</h1>
+        <h1>My Study is: {study}</h1>
+        <h1>My date of Study is: {dateOfStudy}</h1>
+        <h1>My Company Name is: {company}</h1>
+        <h1>My Position Title is: {position}</h1>
+        <h1>The task completed is: {task}</h1>
+        <h1>Date from: {dateFrom}</h1>
+        <h1>Date To: {dateTo}</h1>
       </div>
     );
-  }
-}
+  };
+
+  return (
+    <div>
+        <div>
+          <GenInfo
+            dataName={bringName}
+            dataEmail={bringEmail}
+            dataPhone={bringPhone}
+            state={state}
+          />
+          <EduExp
+            dataSchool={bringSchool}
+            dataStudy={bringStudy}
+            dataDateOfStudy={bringDateOfStudy}
+            state={state}
+          />
+          <PracExp
+            dataCompany={bringCompany}
+            dataPosition={bringPosition}
+            dataTasks={bringTasks}
+            dataDateFrom={bringDateFrom}
+            dataDateTo={bringDateTo}
+            state={state}
+          />
+        </div>
+        <button onClick={handleEdit}>Edit</button>
+        <button onClick={handleSubmit}>Submit</button>
+        {element}
+      </div>
+    ); 
+
+
+};
 
 export default App;
